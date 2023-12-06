52584e62a98bc48428019994e145446160e0fdce
PatriciaRGSacramento <patriciargsacramento@outlook.com>
2023-12-05 21:50:59 -0300

    aula login

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useRouter } from "next/router";
+import { authService } from "../src/services/auth/authService";
 
 export default function HomeScreen() {
   const router = useRouter();
--- frontend/src/services/auth/authService.js
+++ frontend/src/services/auth/authService.js
@@ -1,5 +1,19 @@
 export const authService = {
-  async login(){
-    fetch('http://localhost:4000/api/login')
+  async login({ username, password }){
+    return fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username,
+        password
+      })
+    })
+    .then( async (response) => {
+      if (!response.ok) throw new Error('Usuário ou senha inválidos');
+      const body = await response.json();
+      console.log(body)
+    })
   }
 }
